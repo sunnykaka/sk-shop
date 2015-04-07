9f741064ab1be8b8ffdd253bdbc3c2e5c34cc931
zhb <zhb@yezaoshu.com>
2015-04-07 14:33:24 +0800

    update category

---
+++ product-center/src/main/java/domain/Category.java
@@ -0,0 +1,92 @@
+package domain;
+
+import common.models.utils.EntityClass;
+import common.models.utils.OperableData;
+import org.hibernate.annotations.Type;
+import org.joda.time.DateTime;
+
+import javax.persistence.*;
+
+/**
+ * Created by zhb on 15-4-1.
+ * 后台类目
+ */
+@Table(name = "category")
+@Entity
+public class Category implements EntityClass<Integer>,OperableData {
+
+    /** 类目根目录父ID */
+    public static final Integer PARENT_DEFAULT = -1;
+
+    private Integer id;
+
+    private String name;
+
+    private Integer parentId;
+
+    private DateTime createTime;
+
+    private DateTime updateTime;
+
+    private Integer operatorId;
+
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Column(name = "name")
+    @Basic
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Column(name = "parent_id")
+    @Basic
+    public Integer getParentId() {
+        return parentId;
+    }
+
+    public void setParentId(Integer parentId) {
+        this.parentId = parentId;
+    }
+
+    @Column(name = "create_time")
+    @Type(type="org.jadira.usertype.dateandtime.joda.PersistentDateTime")
+    public DateTime getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(DateTime createTime) {
+        this.createTime = createTime;
+    }
+
+    @Column(name = "update_time")
+    @Type(type="org.jadira.usertype.dateandtime.joda.PersistentDateTime")
+    public DateTime getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(DateTime updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    @Column(name = "operator_id")
+    @Basic
+    public Integer getOperatorId() {
+        return operatorId;
+    }
+
+    public void setOperatorId(Integer operatorId) {
+        this.operatorId = operatorId;
+    }
+}
