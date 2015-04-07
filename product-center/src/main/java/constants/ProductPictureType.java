f6cc3ff5bbe3c373faa8b81a4551e81fd797bc70
zhb <zhb@yezaoshu.com>
2015-04-07 16:55:47 +0800

    update category

--- product-center/src/main/java/constants/ProductPictureType.java
+++ product-center/src/main/java/constants/ProductPictureType.java
@@ -2,7 +2,6 @@ package constants;
 
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.databind.ser.std.EnumSerializer;
-import common.models.utils.ViewEnum;
 
 /**
  * 商品图片类型
@@ -11,21 +10,20 @@ import common.models.utils.ViewEnum;
  * Created by zhb on 15-4-2.
  */
 @JsonSerialize(using = EnumSerializer.class)
-public enum ProductPictureType implements ViewEnum {
+public enum ProductPictureType{
 
     DEFAULT(""),
 
     MAIN("主图");
 
     public String value;
+
     ProductPictureType(String value) {this.value = value;}
 
-    @Override
     public String getName() {
         return this.toString();
     }
 
-    @Override
     public String getValue() {
         return value;
     }
