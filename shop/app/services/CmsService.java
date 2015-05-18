0fcf10f679f901db7c2d110b6c68eb94a23bffa9
amos.zhou <amoszhou@foxmail.com>
2015-05-18 10:53:05 +0800

    重构判断一个商品是否正在首发

--- shop/app/services/CmsService.java
+++ shop/app/services/CmsService.java
@@ -80,9 +80,7 @@ public class CmsService {
      */
     @Transactional(readOnly = true)
     public boolean onFirstPublish(int prodId) {
-        String sql = "select * from cms_exhibition where (beginTime  <= Now() AND endTime > Now())  and  id in ( select exhibitionId from exhibition_item where prodId = ?1)";
-        List<CmsExhibition> list = generalDao.getEm().createNativeQuery(sql, CmsExhibition.class).setParameter(1, prodId).getResultList();
-        return list != null && list.size() > 0;
+        return findExhibitionWithProdId(prodId).isPresent()
     }
 
     public Optional<CmsExhibition>  findExhibitionWithProdId(int prodId){
