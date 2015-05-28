5b472dffbfc0ed9bf7df59fd8f95ba6707abbe2d
lidujun <ldj@yezaoshu.com>
2015-05-28 14:52:00 +0800

    订单号生成修改：改为redis缓存方式

--- order-center/src/main/java/ordercenter/util/OrderNumberUtil.java
+++ order-center/src/main/java/ordercenter/util/OrderNumberUtil.java
@@ -2,7 +2,6 @@ package ordercenter.util;
 
 import common.utils.RedisUtils;
 import org.apache.commons.lang3.math.NumberUtils;
-import play.cache.Cache;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -24,6 +23,20 @@ public class OrderNumberUtil {
     private static final ReentrantLock LOCK = new ReentrantLock();
 
     /**
+     * 订单号由多部分组成, 受限于 Long.MAX_VALUE, 只能有 19 位.<br/>
+     * 1. 年月日, 8 位. <br/>
+     * 2. 自增数, 最多 8 位. <br/>
+     */
+    public static long getOrderNo() {
+        LOCK.lock();
+        try {
+            return NumberUtils.toLong(getStringFromNow() + getCurCacheOrderNo());
+        } finally {
+            LOCK.unlock();
+        }
+    }
+
+    /**
      * 年月日, 异常则返回 空字符串
      */
     public static String getStringFromNow() {
@@ -35,43 +48,14 @@ public class OrderNumberUtil {
     }
 
     /**
-     * 订单号由多部分组成, 受限于 Long.MAX_VALUE, 只能有 19 位.<br/>
-     * 1. 年月日, 8 位. <br/>
-     * 2. 自增数, 最多 8 位. <br/>
-     */
-    public static long getOrderNo() {
-        long curOrderNo = getCurOrderNo();
-        return NumberUtils.toLong(getStringFromNow() + curOrderNo);
-    }
-
-    /**
-     * 获取当前的订单号，且把加1的新值放到缓存中
-     * @return
-     */
-    private static long getCurOrderNo() {
-        LOCK.lock();
-        try {
-            Long curOrderNo = (Long) Cache.get(OrderNumberUtil.CUR_ORDER_NO_KEY);
-            if(curOrderNo == null) {
-                curOrderNo = getCurCacheOrderNo();
-            }
-            Cache.set(OrderNumberUtil.CUR_ORDER_NO_KEY, curOrderNo + 1);
-            return curOrderNo;
-        } finally {
-            LOCK.unlock();
-        }
-    }
-
-    /**
      * 获取当前缓存中的最大的订单值
      */
     public static long getCurCacheOrderNo() {
-        long curValue =  RedisUtils.withJedisClient(jedis -> {
+      return RedisUtils.withJedisClient(jedis -> {
             String key = RedisUtils.buildKey(OrderNumberUtil.CUR_ORDER_NO_KEY);
             jedis.incr(key);
             long value = Long.valueOf(jedis.get(key));
             return value;
         });
-        return curValue;
     }
 }
