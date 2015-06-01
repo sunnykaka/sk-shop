ff15adf30be4862c00e5987b1ff14341fe94f247
lein <xml@yezaoshu.com>
2015-06-01 16:56:54 +0800

    6.1  前端修复收货地址

--- shop/public/js/address.js
+++ shop/public/js/address.js
@@ -199,7 +199,7 @@ $(function(){
 
     //生成地址dom
     function  addressDom (data){
-        return "<li  data-id='"+data.id+"'><div class='receiver'><strong>"+data.name+"<span class='space'></span>收</strong><span class='default' data-id="+data.id+">默认地址</span></div><div class='details-address'>"+
+        return "<li  data-id='"+data.id+"'><div class='receiver'><strong>"+data.name+"<span class='space'></span>收</strong><span class='default' data-id="+data.id+">设置默认地址</span></div><div class='details-address'>"+
         "<p><span class='provice'>"+data.province+"</span><span class='space'></span><span class='city'>"+data.city+"</span><span class='space'></span><span class='area'>"+data.area+"</span></p><p class='location'  title="+data.location+">"+data.location+"</p>"+
         "<p class='phone'>"+data.mobile+"</p></div><div class='edit-address'><span class='edit btn' data-id="+data.id+">修改</span><span class='delete btn' data-id="+data.id+">删除</span></div><span class='current-ico'></span></li>";
     }
@@ -306,6 +306,8 @@ $(function(){
                     if (data.result) {
                        // location.href= '/my/address';
                         item.siblings('.current').find('.edit-address').append("<span class='delete btn' data-id="+defaultId+">删除</span>");
+                        item.siblings('.current').find('.current-ico').hide();
+                        item.find('.current-ico').show();
                         item.addClass('current').siblings('li').find('.default').text('设置默认地址');
                         item.find('.delete').remove();
                         item.addClass('current').siblings('li').removeClass('current');
