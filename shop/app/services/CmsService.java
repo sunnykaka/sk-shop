ca7093e716c540c6f7b7c23c7fcdd079baca21f0
amos.zhou <amoszhou@foxmail.com>
2015-06-04 17:15:27 +0800

    设计师商品列表 BUg修复： 一个商品首发过，已经到期，然后另外一个专场又来首发他是 查询报错

--- shop/app/services/CmsService.java
+++ shop/app/services/CmsService.java
@@ -163,7 +163,7 @@ public class CmsService {
 
     @Transactional(readOnly = true)
     public Optional<CmsExhibition> findExhibitionWithProdId(int prodId) {
-        String sql = "select * from cms_exhibition where (beginTime  <= Now() AND endTime > Now())  and  id = ( select exhibitionId from exhibition_item where prodId = ?1)";
+        String sql = "select * from cms_exhibition where (beginTime  <= Now() AND endTime > Now())  and  id in ( select exhibitionId from exhibition_item where prodId = ?1)";
         List<CmsExhibition> list = generalDao.getEm().createNativeQuery(sql, CmsExhibition.class).setParameter(1, prodId).getResultList();
         if (list != null && list.size() > 0) {
             return Optional.of(list.get(0));
