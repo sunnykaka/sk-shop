7ed902930b9bb8567afb967950e287e4574d268b
lidujun <ldj@yezaoshu.com>
2015-06-11 18:11:26 +0800

    购物车修改

--- shop/app/controllers/shop/CartController.java
+++ shop/app/controllers/shop/CartController.java
@@ -203,7 +203,7 @@ public class CartController extends Controller {
                    cartItems = cartService.queryCarItemsByCartId(cart.getId());
                 }
                 if(cartItems == null || cartItems.size() == 0) {
-                    return ok(new JsonResult(true,"用户当前购物车为空", 0).toNode());
+                    return ok(new JsonResult(true,"用户当前购物车为空", 1).toNode());
                 }
                 int totalNum = 0;
                 for(CartItem cartItem : cartItems) {
