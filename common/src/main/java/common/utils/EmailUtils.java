0cc544a423198e8585fa49a7dd5e2e9e8995a29c
zhb <zhb@yezaoshu.com>
2015-06-23 09:00:06 +0800

    邮件异步发送

--- common/src/main/java/common/utils/EmailUtils.java
+++ common/src/main/java/common/utils/EmailUtils.java
@@ -4,23 +4,34 @@ import play.Play;
 import play.libs.mailer.Email;
 import play.libs.mailer.MailerPlugin;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * Created by zhb on 15-4-30.
  */
 public class EmailUtils {
 
-    public static void sendEmail(String mailTo,String title,String template) {
-        try {
-            Email email = new Email();
+    private static final ExecutorService executor = Executors.newSingleThreadExecutor();
 
-            email.setFrom(Play.application().configuration().getString("smtp.from", "service@yezaoshu.com"));
-            email.addTo(mailTo);
-            email.setSubject(title);
-            email.setBodyHtml(template);
-            MailerPlugin.send(email);
-        }catch (Exception e){
-            play.Logger.error("发送邮件失败",e);
-        }
+    public static void sendEmail(String mailTo,String title,String template) {
+        executor.execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Email email = new Email();
+
+                    email.setFrom(Play.application().configuration().getString("smtp.from", "service@yezaoshu.com"));
+                    email.addTo(mailTo);
+                    email.setSubject(title);
+                    email.setBodyHtml(template);
+
+                    MailerPlugin.send(email);
+                }catch (Exception e){
+                    play.Logger.error("发送邮件失败",e);
+                }
+            }
+        });
 
     }
 
