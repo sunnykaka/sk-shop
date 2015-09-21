a81a611c7c4130c7a1e9533d1e795f209e452190
zhb <zhb@yezaoshu.com>
2015-09-21 17:56:37 +0800

    删除没有用代码

--- shop/app/controllers/user/SendSMSController.java
+++ shop/app/controllers/user/SendSMSController.java
@@ -2,22 +2,9 @@ package controllers.user;
 
 import common.exceptions.AppException;
 import common.utils.JsonResult;
-import common.utils.RegExpUtils;
-import javapns.devices.Device;
-import javapns.devices.implementations.basic.BasicDevice;
-import javapns.notification.AppleNotificationServerBasicImpl;
-import javapns.notification.PushNotificationManager;
-import javapns.notification.PushNotificationPayload;
-import javapns.notification.PushedNotification;
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import play.mvc.Controller;
 import play.mvc.Result;
-import services.IOSPushService;
 import usercenter.domain.SmsSender;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * 通用下发手机短信
@@ -26,9 +13,6 @@ import java.util.List;
  */
 @org.springframework.stereotype.Controller
 public class SendSMSController extends Controller {
-
-    @Autowired
-    IOSPushService iosPushService;
 
     public Result sendSMS(String phone) {
 
@@ -40,13 +24,6 @@ public class SendSMSController extends Controller {
         }
 
         return ok(new JsonResult(true).toNode());
-    }
-
-    public Result IOSPush() {
-
-        iosPushService.push();
-
-        return ok();
     }
 
 }
