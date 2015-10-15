5b57dd656cded23f1b6aed59d95e9b42b1bd595b
zhb <zhb@yezaoshu.com>
2015-10-15 17:11:03 +0800

    微信支付客户端IP改成request获取

--- app/test/controllers/api/shop/OrderAndPayApiControllerTest.java
+++ app/test/controllers/api/shop/OrderAndPayApiControllerTest.java
@@ -37,7 +37,7 @@ public class OrderAndPayApiControllerTest extends BaseTest implements DbTest, Lo
             params.put(UserTokenProvider.ACCESS_TOKEN_KEY, loginResult.getAccessToken());
             Http.RequestBuilder request = new Http.RequestBuilder().
                     method(POST).
-                    uri(controllers.api.shop.routes.AppOrderAndPayController.submitToPay(false, "47772,47773", 96, "Alipay", "", "IOSApp").url()).
+                    uri(controllers.api.shop.routes.AppOrderAndPayController.submitToPay(false, "47772,47773", 96, "Alipay","IOSApp").url()).
                     bodyForm(params);
             result = routeWithExceptionHandle(request);
             if(result.status() != OK) {
