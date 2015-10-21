c643f8300137765ff9cc053a2c10868855f8df33
akka <lb@yezaoshu.com>
2015-10-21 15:11:44 +0800

    添加trade.version字段

--- docs/sql_change/change_20151015.sql
+++ docs/sql_change/change_20151015.sql
@@ -6,3 +6,5 @@ UPDATE apptheme SET startTime = '2015-10-13 10:00:00' WHERE themeNo = 3;
 UPDATE apptheme SET startTime = '2015-10-14 10:00:00' WHERE themeNo = 4;
 
 ALTER TABLE theme_collect ADD COLUMN deviceId VARCHAR(200) DEFAULT NULL;
+
+ALTER TABLE trade ADD COLUMN version int(11) DEFAULT NULL;
