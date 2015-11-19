707e11276187505201e3be655462980befb9095e
zhb <zhb@yezaoshu.com>
2015-11-19 15:40:41 +0800

    评论表sql修改

--- docs/sql_change/change_20151116.sql
+++ docs/sql_change/change_20151116.sql
@@ -1,3 +1,7 @@
 ALTER TABLE limitedtimediscount
 ADD COLUMN `discountName` VARCHAR(45) NULL COMMENT '' ,
 ADD COLUMN `handleStatus` INT NULL COMMENT '' ;
+
+ALTER TABLE valuation
+ADD COLUMN `replyUserId` int(11) DEFAULT '0',
+ADD COLUMN `replyUserName` varchar(128) DEFAULT NULL;
