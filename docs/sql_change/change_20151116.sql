f23cebcb87959f36ad960b52ed8c04b48deb406e
zhb <zhb@yezaoshu.com>
2015-11-20 16:54:22 +0800

    增加活动代金券关联表

--- docs/sql_change/change_20151116.sql
+++ docs/sql_change/change_20151116.sql
@@ -5,3 +5,13 @@ ADD COLUMN `handleStatus` INT NULL COMMENT '' ;
 ALTER TABLE valuation
 ADD COLUMN `replyUserId` int(11) DEFAULT '0',
 ADD COLUMN `replyUserName` varchar(128) DEFAULT NULL;
+
+DROP TABLE IF EXISTS `voucher_activity`;
+CREATE TABLE `voucher_activity` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `uniqueNo` varchar(128) NOT NULL,
+  `activityId` int(11) NOT NULL,
+  `userId` int(11) NOT NULL,
+  `createTime` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COMMENT='活动与代金券关联';
